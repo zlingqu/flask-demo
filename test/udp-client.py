b3d9fce5bca4a3a7503d3ef9740a5fc93d6bb42c
link <zhongling.qu@ehang.com>
2025-01-13 15:34:21 +0800

    debug

--- test/udp-client.py
+++ test/udp-client.py
@@ -9,7 +9,7 @@ while i < 10000:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # 发送数据:
     print('send   :', i)
-    s.sendto(str(i).encode('utf-8'), ('aav.ehang.com', 9999))
+    s.sendto(str(i).encode('utf-8'), ('127.0.0.1', 9999))
     print('recvice:', s.recv(1024).decode('utf-8'))
     # 接收数据:
     i += 1
